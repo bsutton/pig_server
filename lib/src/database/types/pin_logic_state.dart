e4764206e5ad28cea9b1d267ad3f1755beefbb69
Brett Sutton <bsutton@noojee.com.au>
2025-06-01 11:59:19 +1000

    renaemd pin_logic status to pin logic state.

--- lib/src/database/types/pin_logic_state.dart
+++ lib/src/database/types/pin_logic_state.dart
@@ -1,16 +1,16 @@
 import 'package:pig_common/pig_common.dart';
 
 /// Represents the status of a GPIO pin.
-enum PinLogicStatus {
+enum PinLogicState {
   on,
   off;
 
   /// Determines the status of a pin based on its activation type and state.
-  static PinLogicStatus getStatus(EndPoint pin, {required bool isHigh}) {
+  static PinLogicState getStatus(EndPoint pin, {required bool isHigh}) {
     if (pin.activationType == PinActivationType.lowIsOn) {
-      return isHigh ? PinLogicStatus.off : PinLogicStatus.on;
+      return isHigh ? PinLogicState.off : PinLogicState.on;
     } else {
-      return isHigh ? PinLogicStatus.on : PinLogicStatus.off;
+      return isHigh ? PinLogicState.on : PinLogicState.off;
     }
   }
 }
