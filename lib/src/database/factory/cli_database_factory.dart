6961c11de28b2ae4f03605368dd72ed6e530fb23
Brett Sutton <bsutton@noojee.com.au>
2025-12-28 09:22:30 +1100

    sqfliteFFiInit is required for non-mobile platforms.

--- lib/src/database/factory/cli_database_factory.dart
+++ lib/src/database/factory/cli_database_factory.dart
@@ -5,6 +5,8 @@ import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'pig_database_factory.dart' as local;
 
 class CliDatabaseFactory implements local.PigDatabaseFactory {
+  static CliDatabaseFactory? instance;
+
   factory CliDatabaseFactory() {
     if (instance == null) {
       instance = CliDatabaseFactory._();
@@ -15,11 +17,11 @@ class CliDatabaseFactory implements local.PigDatabaseFactory {
   }
 
   CliDatabaseFactory._();
-  static CliDatabaseFactory? instance;
 
   void initDatabaseFactory() {
     if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
       /// required for non-mobile platforms.
+      sqfliteFfiInit();
       databaseFactory = databaseFactoryFfi;
     } else if (Platform.isAndroid || Platform.isIOS) {
       /// uses the default factory.
